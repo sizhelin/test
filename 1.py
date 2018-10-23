54aceaa3c590c3688dcc8bf6346024e6e84a8dd0
linsizhe <sizhelin@aliyun.com>
2018-10-23 21:58:04 +0800

    加入循环结构体

--- 1.py
+++ 1.py
@@ -1,8 +1,15 @@
-x=input("请输入一个整数判断是否为素数：")
-x=int (x)
-for i in range(2,x):
-    if x % i == 0:
-        print ("不是")
-        break
-else:
-    print ("是")
+ch = ""
+
+while ch !="q":
+
+
+	x=input("请输入一个整数判断是否为素数：")
+	x=int (x)
+	for i in range(2,x):
+	    if x % i == 0:
+	        print ("不是")
+	        break
+	else:
+	    print ("是")
+	    
+    ch = input("quit?")
